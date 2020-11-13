5fc208aa80e07c85df1cef7203a4465d9639993e
samuel.jansen <samuel.jansenn@gmail.com>
2020-11-13 05:38:07 -0300

    feat: globals static package updated

--- setup.py
+++ setup.py
@@ -1,58 +1,55 @@
 from distutils.core import setup
 import os
 
-version = '0.0.1-82'
-name = 'python_framework'
-url = f'https://github.com/SamuelJansen/{name}/'
+VERSION = '0.0.1-83'
+NAME = 'python_framework'
+URL = f'https://github.com/SamuelJansen/{NAME}/'
 
 OS_SEPARATOR = os.path.sep
-
-# distPackage = f'lib{OS_SEPARATOR}site-packages'
-distPackage = f'statics'
-
-swaggerRelativePath = f'{OS_SEPARATOR}api{OS_SEPARATOR}resource{OS_SEPARATOR}swaggerui'
+SWAGGER_RELATIVE_PATH = f'{OS_SEPARATOR}api{OS_SEPARATOR}resource{OS_SEPARATOR}swaggerui'
+STATIC_PACKAGE = 'statics'
 
 setup(
-    name = name,
+    name = NAME,
     packages = [
-        name,
-        f'{name}{OS_SEPARATOR}api',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}annotation',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}model',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}domain',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}helper',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service{OS_SEPARATOR}openapi',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service{OS_SEPARATOR}flask',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}resource',
-        f'{name}{OS_SEPARATOR}api{OS_SEPARATOR}resource{OS_SEPARATOR}swaggerui'
+        NAME,
+        f'{NAME}{OS_SEPARATOR}api',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}annotation',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}model',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}domain',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}helper',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service{OS_SEPARATOR}openapi',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}service{OS_SEPARATOR}flask',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}resource',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}resource{OS_SEPARATOR}swaggerui'
     ],
     data_files = [
-        (f'{distPackage}{OS_SEPARATOR}{name}{swaggerRelativePath}', [
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}favicon-16x16.png',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}favicon-32x32.png',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}index.template.html',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}oauth2-redirect.html',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui-bundle.js',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui-bundle.js.map',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui-standalone-preset.js',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui-standalone-preset.js.map',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui.css',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui.css.map',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui.js',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}swagger-ui.js.map',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}LICENSE',
-            f'{name}{swaggerRelativePath}{OS_SEPARATOR}VERSION'
+        (f'{STATIC_PACKAGE}{OS_SEPARATOR}{NAME}{SWAGGER_RELATIVE_PATH}', [
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}favicon-16x16.png',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}favicon-32x32.png',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}index.template.html',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}oauth2-redirect.html',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui-bundle.js',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui-bundle.js.map',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui-standalone-preset.js',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui-standalone-preset.js.map',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui.css',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui.css.map',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui.js',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}swagger-ui.js.map',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}LICENSE',
+            f'{NAME}{SWAGGER_RELATIVE_PATH}{OS_SEPARATOR}VERSION'
         ])
     ],
-    version = version,
+    version = VERSION,
     license = 'MIT',
     description = 'Flask wrapper',
     author = 'Samuel Jansen',
     author_email = 'samuel.jansenn@gmail.com',
-    url = url,
-    download_url = f'{url}archive/v{version}.tar.gz',
+    url = URL,
+    download_url = f'{URL}archive/v{VERSION}.tar.gz',
     keywords = ['flask', 'sqlalchemy', 'open api', 'jwt', 'serializer'],
     install_requires = [
         'gunicorn==20.0.4',
@@ -64,7 +61,7 @@ setup(
         'Flask-Swagger-Ui==3.36.0',
         'psycopg2-binary==2.8.6',
         'SQLAlchemy==1.3.20',
-        'globals==0.0.43-15',
+        'globals==0.0.43-25',
         'python_helper==0.0.14'
     ],
     classifiers = [
