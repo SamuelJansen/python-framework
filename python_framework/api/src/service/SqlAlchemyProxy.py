fe3ff9852b9d17a399dba124665e558494e85d44
samuel.jansen <samuel.jansenn@gmail.com>
2021-03-11 20:12:25 -0300

    feat: Date and Time added to SqlAlchemyProxy

--- python_framework/api/src/service/SqlAlchemyProxy.py
+++ python_framework/api/src/service/SqlAlchemyProxy.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, exists, select
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 from sqlalchemy.orm.collections import InstrumentedList
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
-from sqlalchemy import Table, Column, Integer, String, Float, ForeignKey, UnicodeText, MetaData, Sequence, DateTime
+from sqlalchemy import Table, Column, Integer, String, Float, ForeignKey, UnicodeText, MetaData, Sequence, DateTime, Date, Time
 from sqlalchemy import and_, or_
 from sqlalchemy.sql.expression import literal
 
