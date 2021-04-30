35e35d8e586db714a2a4ca1018f96ec555850512
samuel.jansen <samuel.jansenn@gmail.com>
2021-04-30 18:44:11 -0300

    feat: New annotations

--- python_framework/__init__.py
+++ python_framework/__init__.py
@@ -34,3 +34,8 @@ from python_framework.api.src.annotation.EnumAnnotation import *
 from python_framework.api.src.service.flask.FlaskManager import *
 from python_framework.api.src.annotation.SchedulerAnnotation import *
 from python_framework.api.src.annotation.ServiceAnnotation import *
+from python_framework.api.src.annotation.RepositoryAnnotation import *
+from python_framework.api.src.annotation.ValidatorAnnotation import *
+from python_framework.api.src.annotation.MapperAnnotation import *
+from python_framework.api.src.annotation.ConverterAnnotation import *
+from python_framework.api.src.annotation.HelperAnnotation import *
