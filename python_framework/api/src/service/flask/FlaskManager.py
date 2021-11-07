3ee53cbd0b81136756b4faecec67e85b59278265
samuel.jansen <samuel.jansenn@gmail.com>
2021-11-07 19:12:24 -0300

    feature: closing repository ate exit app

--- python_framework/api/src/service/flask/FlaskManager.py
+++ python_framework/api/src/service/flask/FlaskManager.py
@@ -105,9 +105,8 @@ def newApp(
     try:
         app = globals.importResource(KW_APP, resourceModuleName=globalsInstance.apiName, required=True)
     except Exception as exception:
-        apiName = globalsInstance.getApiSetting(lobals.AttributeKey.API_NAME)
         apiPath = f'{c.DOT}{EnvironmentHelper.OS_SEPARATOR}{globalsInstance.BASE_API_PATH}{globals.AttributeKey.API_NAME}.py'
-        errorMessage = f"Not possible to load app. Make shure it's name is properlly configured at '{apiName}' and it's instance is named 'app' at '{apiPath}'"
+        errorMessage = f"Not possible to load app. Make shure it's name is properlly configured at '{globalsInstance.apiName}' and it's instance is named 'app' at '{apiPath}'"
         log.error(newApp, errorMessage, exception)
         raise exception
     if ObjectHelper.isNone(app):
