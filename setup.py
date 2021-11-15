4061e827220b1a798d483868ed3a24da9681734a
samuel.jansen <samuel.jansenn@gmail.com>
2021-11-15 14:10:52 -0300

    feature: http client implementation

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sudo rm /usr/bin/pip
 sudo ln -s /usr/local/bin/pipX.Y /usr/bin/pip
 ''')
 
-VERSION = '0.2.209.11'
+VERSION = '0.2.209.12'
 
 NAME = 'python_framework'
 URL = f'https://github.com/SamuelJansen/{NAME}/'
