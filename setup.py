f3d714c62f60a7ac7968c2a62e81cf5aa3e93964
samuel.jansen <samuel.jansenn@gmail.com>
2021-11-15 13:15:02 -0300

    feature: http client implementation

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sudo rm /usr/bin/pip
 sudo ln -s /usr/local/bin/pipX.Y /usr/bin/pip
 ''')
 
-VERSION = '0.2.209'
+VERSION = '0.2.209.1'
 
 NAME = 'python_framework'
 URL = f'https://github.com/SamuelJansen/{NAME}/'
@@ -27,6 +27,7 @@ setup(
         f'{NAME}{OS_SEPARATOR}api',
         f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src',
         f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}annotation',
+        f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}annotation{OS_SEPARATOR}client',
         f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}model',
         f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}util',
         f'{NAME}{OS_SEPARATOR}api{OS_SEPARATOR}src{OS_SEPARATOR}constant',
