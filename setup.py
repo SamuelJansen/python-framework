37207f6089ecfb177319b23b529cbd11e9148fed
samuel.jansen <samuel.jansenn@gmail.com>
2022-02-21 02:47:15 -0300

    fix: returning clientResponse in getCompleteClientResponse

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sudo rm /usr/bin/pip
 sudo ln -s /usr/local/bin/pipX.Y /usr/bin/pip
 ''')
 
-VERSION = '0.2.226'
+VERSION = '0.2.227'
 
 NAME = 'python_framework'
 URL = f'https://github.com/SamuelJansen/{NAME}/'
